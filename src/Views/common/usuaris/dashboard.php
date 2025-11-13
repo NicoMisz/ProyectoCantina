61c9bcacad34c83ab1f4622e3305ada77865fee3
NicoMisz <nicommisz@gmail.com>
2025-11-13 16:02:21 +0100

    quitar bootstrap css

--- src/Views/common/usuaris/dashboard.php
+++ src/Views/common/usuaris/dashboard.php
@@ -9,17 +9,26 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Form Usuario</title>
+    <link rel="stylesheet" href="/css/main.css">
 </head>
 
 <body>
-    <a onclick="visualizarPerfil()">Perfil</a>
+    <header class="header nav">
+        <a href="/dashboard"> <img class="logo-header" src="/assets/media/E.png" alt="Logo cantina tony's"></a>
+        <ul>
+            <li>
+                <ul class="menu-dropdown">
+                    <li><a href="/catalogo">El catalogo</a></li>
+                    <li><a href="/aboutUs">About us</a></li>
+                    <li><a href="/formulari">Contacto</a></li>
+                </ul>
+            </li>
+            <li><a href="/carrito">Carrito</a></li>
+            <li><a href="/perfil">Perfil</a></li>
+        </ul>
+        <hr>
+    </header>
 
-
-    <script>
-        function visualizarPerfil() {
-            window.location.href = '/perfil';
-        };
-    </script>
 
 </body>
 
