99db3f18c883efaa14fcd97cda1d6a51f035f3b0
Goulthar <johnesparrell70@gmail.com>
2025-11-28 17:43:11 +0100

    carrito?

--- public/assets/js/carritoDEV.js
+++ public/assets/js/carritoDEV.js
@@ -221,7 +221,6 @@ function actualitzarLlistaCarreto(carreto) {
     const contenidoHTML = `
     <div class="cart-header">
         <h4 style="margin: 0;">Mi Carrito</h4>
-        <button class="cart-close-btn" id="closeCart" aria-label="Cerrar carrito">×</button>
     </div>
     <div class="cart-content">
         <div class="cart-items">
